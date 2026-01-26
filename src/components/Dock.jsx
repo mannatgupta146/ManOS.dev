abe0d45e7c045534ff7a1be7eb753d17d525abe2
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-26 20:07:20 +0530

    icons added and dock part done

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import "./Dock.scss"
 
 const Dock = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className='dock'>
+        <div className="icon cli"><img src="/icons/cli.svg" alt="" /></div>
+        <div className="icon calender"><img src="/icons/calender.svg" alt="" /></div>
+        <div className="icon mail"><img src="/icons/mail.svg" alt="" /></div>
+        <div className="icon github"><img src="/icons/github.svg" alt="" /></div>
+        <div className="icon pdf"><img src="/icons/pdf.svg" alt="" /></div>
+        <div className="icon note"><img src="/icons/note.svg" alt="" /></div>
+        <div className="icon link"><img src="/icons/link.svg" alt="" /></div>
+        <div className="icon spotify"><img src="/icons/spotify.svg" alt="" /></div>
+    </footer>
   )
 }
 
