69452f31270b1e039bc7011b17a85d10b9ea60a8
mannatgupta146 <mannatgupta146@gmail.com>
2026-02-05 03:12:59 +0530

    nav left done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.scss'
 import Dock from './components/Dock'
+import Nav from './components/Nav'
 
 const App = () => {
   return (
     <main>
+      <Nav />
       <Dock />
     </main>
   )
