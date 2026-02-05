3ee2434d21f531581ad009deaf23711ed6fefafa
mannatgupta146 <mannatgupta146@gmail.com>
2026-02-05 19:30:29 +0530

    rdn and macwindow

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.scss'
 import Dock from './components/Dock'
 import Nav from './components/Nav'
+import MacWindow from './components/windows/MacWindow'
 
 const App = () => {
   return (
     <main>
       <Nav />
       <Dock />
+      <MacWindow />
     </main>
   )
 }
