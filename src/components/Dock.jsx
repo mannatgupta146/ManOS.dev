ad0774eac34c7dbdcd8f0991a73e84e207cd5e65
mannatgupta146 <mannatgupta146@gmail.com>
2026-02-08 19:22:03 +0530

    dock hover

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -4,14 +4,37 @@ import "./Dock.scss"
 const Dock = () => {
   return (
     <footer className='dock'>
-        <div className="icon cli-icon"><img src="/icons/cli.svg" alt="" /></div>
-        <div className="icon calender-icon"><img src="/icons/calender.svg" alt="" /></div>
-        <div className="icon mail-icon"><img src="/icons/mail.svg" alt="" /></div>
-        <div className="icon github-icon"><img src="/icons/github.svg" alt="" /></div>
-        <div className="icon pdf-icon"><img src="/icons/pdf.svg" alt="" /></div>
-        <div className="icon note-icon"><img src="/icons/note.svg" alt="" /></div>
-        <div className="icon code-icon"><img src="/icons/code.png" alt="" /></div>
-        <div className="icon spotify-icon"><img src="/icons/spotify.svg" alt="" /></div>
+      <div className="icon cli-icon" data-label="Terminal">
+        <img src="/icons/cli.svg" alt="" />
+      </div>
+
+      <div className="icon calender-icon" data-label="Calendar">
+        <img src="/icons/calender.svg" alt="" />
+      </div>
+
+      <div className="icon mail-icon" data-label="Mail">
+        <img src="/icons/mail.svg" alt="" />
+      </div>
+
+      <div className="icon github-icon" data-label="GitHub">
+        <img src="/icons/github.svg" alt="" />
+      </div>
+
+      <div className="icon pdf-icon" data-label="Resume">
+        <img src="/icons/pdf.svg" alt="" />
+      </div>
+
+      <div className="icon note-icon" data-label="Notes">
+        <img src="/icons/note.svg" alt="" />
+      </div>
+
+      <div className="icon code-icon" data-label="Code Editor">
+        <img src="/icons/code.png" alt="" />
+      </div>
+
+      <div className="icon spotify-icon" data-label="Spotify">
+        <img src="/icons/spotify.svg" alt="" />
+      </div>
     </footer>
   )
 }
